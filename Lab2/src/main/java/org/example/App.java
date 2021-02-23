6f4fd13720fd762322476e7733443209ffc61205
Ecaterina-Hrib <59749220+Ecaterina-Hrib@users.noreply.github.com>
2021-02-23 13:02:43 +0200

    Update App.java

--- Lab2/src/main/java/org/example/App.java
+++ Lab2/src/main/java/org/example/App.java
@@ -8,7 +8,8 @@ public class App
 
 
     public static void main( String[] args )
-    {
+    { // in clasa Problem am inclus clasa Destination + Source
+     // iar in clasa Source am integrat clasa SourceType
 
        Problem p1 = new Problem();
        p1.showProblem();
