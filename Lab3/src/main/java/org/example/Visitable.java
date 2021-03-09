98607e0042e9abbf7f4923f733bd50302845acb6
Ecaterina-Hrib <59749220+Ecaterina-Hrib@users.noreply.github.com>
2021-03-09 10:21:20 +0200

    Update Visitable.java

--- Lab3/src/main/java/org/example/Visitable.java
+++ Lab3/src/main/java/org/example/Visitable.java
@@ -1,4 +1,20 @@
 package org.example;
 // interfata Visitable
 public interface Visitable {
+  LocalTime getOpeningTime();
+  LocalTime getClosingTime();
+    default LocalTime OpeningTime(){
+        LocalTime open = LocalTime.of(9,30);
+        return open;
+    }
+    default LocalTime ClosingTime(){
+        LocalTime close = LocalTime.of(20,00);
+        return close;
+    }
+    public static Duration getVisitingDuration(LocalTime close, LocalTime open)
+    {
+
+      return Duration.between(open,close);
+
+    }
 }
