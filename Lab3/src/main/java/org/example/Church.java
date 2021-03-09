ace2ebef20afa1e006a9cf9e4c80a026f74e13cd
Ecaterina-Hrib <59749220+Ecaterina-Hrib@users.noreply.github.com>
2021-03-09 10:17:31 +0200

    Update Church.java

--- Lab3/src/main/java/org/example/Church.java
+++ Lab3/src/main/java/org/example/Church.java
@@ -1,6 +1,48 @@
 package org.example;
-// clasa Church este doar Visitable, deoarece obiectele de acest tip se identifica doar prin visitable
-public class Church extends Location implements Visitable{
-    public Church() {
+
+import java.time.Duration;
+import java.time.LocalTime;
+
+
+public class Church extends Location implements Visitable {
+    private LocalTime openingTime, closingTime;
+    private Duration duration;
+
+    public Duration getDuration() {
+        return duration;
+    }
+
+    /**
+     * <p>Am folosit un obiect de tip Duration pentru a afla durata vizitabila </p>
+     */
+    public void setDuration() {
+        this.duration = Visitable.getVisitingDuration(openingTime, closingTime);
+    }
+
+
+    @Override
+    public LocalTime getOpeningTime() {
+        return openingTime;
+    }
+
+    public void setOpeningTime(LocalTime openingTime) {
+        this.openingTime = openingTime;
+    }
+
+    @Override
+    public LocalTime getClosingTime() {
+        return closingTime;
+    }
+
+    public void setClosingTime(LocalTime closingTime) {
+        this.closingTime = closingTime;
+    }
+
+    @Override
+    public String toString() {
+        return "Location with a opening time "
+                + openingTime +
+                " and a closing time " + closingTime + " : " + getName();
+
     }
 }
