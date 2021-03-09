b360120134cc86264cc1bc370488a95ab4174f5a
Ecaterina-Hrib <59749220+Ecaterina-Hrib@users.noreply.github.com>
2021-03-09 10:17:53 +0200

    Update City.java

--- Lab3/src/main/java/org/example/City.java
+++ Lab3/src/main/java/org/example/City.java
@@ -1,21 +1,47 @@
 package org.example;
 
+import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Collections;
 
 public class City {
     // lista de locatii + settere, gettere si constructor
+    List<Location> CityLocations = new ArrayList<>();
+
     public City(List<Location> cityLocations) {
         CityLocations = cityLocations;
     }
 
-    List<Location> CityLocations = new ArrayList<>();
+    public void setCityLocations(List<Location> cityLocations) {
+        CityLocations = cityLocations;
+    }
 
     public List<Location> getCityLocations() {
         return CityLocations;
     }
 
-    public void setCityLocations(List<Location> cityLocations) {
-        CityLocations = cityLocations;
+    /**
+     * <p>Functia Visit Hour verifica daca o locatie este Visitable dar nu Payable, si in functie de ora, se sorteaza</p>
+     */
+    public void VisitHour() {
+        List<Location> vizitabile = new ArrayList<>();
+        for (int i = 0; i < this.CityLocations.size(); i++)
+            if (CityLocations.get(i) instanceof Visitable)
+                if (!(CityLocations.get(i) instanceof Payable)) {
+                    vizitabile.add((CityLocations.get(i)));
+                }
+        vizitabile.sort(Comparator.comparing(Location::toString));
+        for (int i = 0; i < vizitabile.size(); i++)
+            System.out.println(vizitabile.get(i));
+    }
+
+
+    @Override
+    public String toString() {
+        return "City{" +
+                "CityLocations=" + CityLocations +
+                '}';
     }
 }
