ab96899493a69a18d895b2d5657e6f012c4e657c
Ecaterina-Hrib <59749220+Ecaterina-Hrib@users.noreply.github.com>
2021-03-09 10:19:09 +0200

    Update Hotel.java

--- Lab3/src/main/java/org/example/Hotel.java
+++ Lab3/src/main/java/org/example/Hotel.java
@@ -1,6 +1,58 @@
 package org.example;
+
+import java.time.LocalTime;
+import java.util.Map;
+
 // clasa hotel implemeteaza clasa interfata Visitable,Payable,Classifiable pentru diferitele valori pe care le va avea obiectul de tip Hotel
 public class Hotel extends Location implements Visitable,Payable,Classifiable{
-    public Hotel() {
+    private LocalTime openingTime, closingTime;
+    private double ticketPrice;
+    private int stars;
+  public Hotel()
+  {
+
+  }
+
+    public void setTicketPrice(double ticketPrice) {
+        this.ticketPrice = ticketPrice;
+    }
+
+    public void setStars(int stars) {
+        this.stars = stars;
+    }
+
+    @Override
+    public int getStars() {
+        return stars;
+    }
+
+    public Hotel(String name, String description, Map<Location, Integer> cost, LocalTime openingTime, LocalTime closingTime, double ticketPrice) {
+        super(name, description, cost);
+        this.openingTime = openingTime;
+        this.closingTime = closingTime;
+        this.ticketPrice = ticketPrice;
+    }
+
+    @Override
+    public LocalTime getOpeningTime() {
+        return openingTime;
+    }
+    @Override
+    public LocalTime getClosingTime() {
+        return closingTime;
+    }
+
+    public void setOpeningTime(LocalTime openingTime) {
+        this.openingTime = openingTime;
+    }
+
+    public void setClosingTime(LocalTime closingTime) {
+        this.closingTime = closingTime;
+    }
+
+
+    @Override
+    public double getTicketPrice() {
+        return ticketPrice;
     }
 }
