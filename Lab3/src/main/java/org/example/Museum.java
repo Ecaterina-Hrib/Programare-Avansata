17d7a8b7ecab6015c57b7e121c0d6aacf47a80e1
Ecaterina-Hrib <59749220+Ecaterina-Hrib@users.noreply.github.com>
2021-03-09 10:19:48 +0200

    Update Museum.java

--- Lab3/src/main/java/org/example/Museum.java
+++ Lab3/src/main/java/org/example/Museum.java
@@ -1,7 +1,41 @@
 package org.example;
-// clasa Museum implemeteaza clasa interfata Visitable,Payable pentru diferitele valori pe care le va avea obiectul de tip Museum
-// deoarece am considerat ca un obiect de tip Museum nu va fi clasificat intr-un anumit fel
-public class Museum extends Location implements Visitable,Payable{
-    public Museum() {
+
+import java.time.LocalTime;
+
+public class Museum extends Location implements Visitable, Payable {
+    /**
+     * <p>clasa Museum implemeteaza clasa interfata Visitable,Payable pentru diferitele valori pe care le va avea obiectul de tip Museum</p>
+     * <p> deoarece am considerat ca un obiect de tip Museum nu va fi clasificat intr-un anumit fel</p>
+     */
+    private LocalTime openingTime, closingTime;
+    private double ticketPrice;
+
+    @Override
+    public LocalTime getOpeningTime() {
+        return openingTime;
     }
+
+    public void setOpeningTime(LocalTime openingTime) {
+        this.openingTime = openingTime;
+    }
+
+    @Override
+    public LocalTime getClosingTime() {
+        return closingTime;
+    }
+
+    public void setClosingTime(LocalTime closingTime) {
+        this.closingTime = closingTime;
+    }
+
+    @Override
+    public double getTicketPrice() {
+        return ticketPrice;
+    }
+
+    public void setTicketPrice(double ticketPrice) {
+        this.ticketPrice = ticketPrice;
+    }
+
+
 }
