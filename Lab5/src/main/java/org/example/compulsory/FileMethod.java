fdbd7a517485a9a1c8ead67be5bb23863c609016
Ecaterina-Hrib <59749220+Ecaterina-Hrib@users.noreply.github.com>
2021-03-16 12:27:42 +0200

    Update FileMethod.java

--- Lab5/src/main/java/org/example/compulsory/FileMethod.java
+++ Lab5/src/main/java/org/example/compulsory/FileMethod.java
@@ -1,15 +1,13 @@
 package org.example.compulsory;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.IOException;
-import java.nio.file.Path;
+
 /**
  * <p>Am folosit interfata pentru metodele standard folosite</p>
  */
 public interface FileMethod {
-    public void add(Item item);
+    public void add(Picture item);
     public void list();
     public void play(int index) ;
     public void save(FileOutputStream way);
