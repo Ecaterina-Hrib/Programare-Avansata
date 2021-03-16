792007fd6b0dc23d25e88ff3e13be80bccc2260b
Ecaterina-Hrib <59749220+Ecaterina-Hrib@users.noreply.github.com>
2021-03-16 12:27:17 +0200

    Update Catalog.java

--- Lab5/src/main/java/org/example/compulsory/Catalog.java
+++ Lab5/src/main/java/org/example/compulsory/Catalog.java
@@ -1,109 +1,49 @@
-package org.example.compulsory;
+package org.example;
+import org.example.compulsory.Catalog;
+import org.example.compulsory.Item;
+import org.example.compulsory.Picture;
 
-import java.awt.*;
-import java.io.*;
-import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.nio.file.Paths;
 /**
- * <p>Clasa Catalog contine un array de itemuri ce indeplineste anumite commands: save, load,list,play,etc</p>
+ * <p>Am formatat 2 ietmuri, le-am adaugat in catalog apoi am facut operatii de tip catalog pe ele</p>
+ *
  */
-public class Catalog implements FileMethod{
+public class App 
+{
+    public static void main( String[] args ) {
+        Picture item = new Picture();
+        Picture item2 = new Picture();
+        item2.setName("best.jpg");
+        item.setName("rota.png");
+        item.setPath(Paths.get("D:\\_ user ecaaa\\Desktop"));
+        item2.setPath(Paths.get("D:\\_ user ecaaa\\Desktop"));
+      Catalog first = new Catalog();
+      first.add(item);
+        first.add(item2);
+        first.play(0);
+        first.play(1);
+        first.list();
 
-   private List<Item> list = new ArrayList<>();
-   public Catalog(){
 
-   }
-    public Catalog(List<Item> list) {
-        this.list = list;
-    }
-
-    @Override
-    public void add(Item item) {
-        list.add(item);
-    }
-
-    @Override
-    public void list() {
-      for(Item s: list){
-          System.out.println(s.getName());
-      }
-
-    }
-
-    @Override
-    public void play(int index)  {
-        File file = new File(list.get(index).getPath()+"\\"+list.get(index).getName());
-       Desktop desktop = Desktop.getDesktop();
         try {
-            desktop.open(file);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-    }
-
-    @Override
-    public void save(FileOutputStream way)  {
-
-                  for(Item s: list){
-                      ObjectOutputStream out = null;
-                      try {
-                           out = new ObjectOutputStream(way);
-
-                          out.writeObject(new Item());
-
-                          out.writeObject(s);
-                      } catch (IOException e) {
-                          e.printStackTrace();
-                      }
-                  }
-        try {
-            way.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    public void load(FileInputStream way)  {
-
-        ObjectInputStream in = null;
-        try {
-            in = new ObjectInputStream(way);
-        } catch (IOException e) {
+            FileOutputStream  out = new FileOutputStream("catalog.txt");
+            first.save(out);
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
         try {
-            Item date = (Item)in.readObject();
-            String message = (String)in.readObject();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
+            FileInputStream in = new FileInputStream("catalog.txt");
+            first.load(in);
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-        list.clear();
-        while(true) {
-            try {
-                if (!list.add((Item)in.readObject())) break;
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (ClassNotFoundException e) {
-                e.printStackTrace();
-            }
-            ;
-        }
 
 
 
-    }
 
-    @Override
-    public String toString() {
-        return "Catalog\n" +
-                "list= " + list + "\n"
-                ;
+
     }
 }
