c29127ae1a1f667ef68b68bd1da09f7bafa22be7
Ecaterina-Hrib <59749220+Ecaterina-Hrib@users.noreply.github.com>
2021-03-23 09:57:35 +0200

    Update App.java

--- Lab5/src/main/java/org/example/App.java
+++ Lab5/src/main/java/org/example/App.java
@@ -1,49 +1,101 @@
 package org.example;
-import org.example.compulsory.Catalog;
-import org.example.compulsory.Item;
-import org.example.compulsory.Picture;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.nio.file.Paths;
+import org.example.classes.Catalog;
+import org.example.classes.CatalogUtil;
+import org.example.classes.Item;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 /**
- * <p>Am formatat 2 ietmuri, le-am adaugat in catalog apoi am facut operatii de tip catalog pe ele</p>
+ * Shell+Interfaces-optional + compuslory
  *
  */
 public class App 
 {
-    public static void main( String[] args ) {
-        Picture item = new Picture();
-        Picture item2 = new Picture();
-        item2.setName("best.jpg");
-        item.setName("rota.png");
-        item.setPath(Paths.get("D:\\_ user ecaaa\\Desktop"));
-        item2.setPath(Paths.get("D:\\_ user ecaaa\\Desktop"));
-      Catalog first = new Catalog();
-      first.add(item);
-        first.add(item2);
-        first.play(0);
-        first.play(1);
-        first.list();
+    public static void main(String[] args) throws IOException, CatalogUtil.InvalidCatalogException, CatalogUtil.InvalidNameException {
+        App app = new App();
+        String commandLine;
+        app.testCreateSave();
+        app.testLoadView();
+        String command;
+        BufferedReader console = new BufferedReader
+                (new InputStreamReader(System.in));
+        while(true)
+        {  System.out.print("shell>");
+            commandLine = console.readLine();
+            if (commandLine.equals("save")){
+                System.out.print("save: ");
+                commandLine = console.readLine();
+                Item item = new Item();
+                item.setId(commandLine);
+                commandLine = console.readLine();
+                item.setName(commandLine);
+                commandLine = console.readLine();
+                item.setLocation(commandLine);
 
+            }
+          else if(commandLine.equals("load"))
+            {
+                System.out.print("load: ");
+                commandLine = console.readLine();
+                Item item = new Item();
+                item.setId(commandLine);
+                commandLine = console.readLine();
+                item.setName(commandLine);
+                commandLine = console.readLine();
+                item.setLocation(commandLine);
 
-        try {
-            FileOutputStream  out = new FileOutputStream("catalog.txt");
-            first.save(out);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            }
+          else if(commandLine.equals("add"))
+            {
+                System.out.print("add: ");
+                commandLine = console.readLine();
+                Item item = new Item();
+                item.setId(commandLine);
+                commandLine = console.readLine();
+                item.setName(commandLine);
+                commandLine = console.readLine();
+                item.setLocation(commandLine);
+            }
+            else if(commandLine.equals("list"))
+            {
+                System.out.print("list: ");
+                commandLine = console.readLine();
+                Item item = new Item();
+                item.setId(commandLine);
+                commandLine = console.readLine();
+                item.setName(commandLine);
+                commandLine = console.readLine();
+                item.setLocation(commandLine);
+            }
+            else if(commandLine.equals("exit"))
+            {
+                System.out.print("exit");
+                continue;
+
+            }
         }
-        try {
-            FileInputStream in = new FileInputStream("catalog.txt");
-            first.load(in);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
 
 
 
+    }
+    private void testCreateSave() throws IOException, CatalogUtil.InvalidNameException {
+        Catalog catalog =
+                new Catalog("Pictures", "D:\\_ user ecaaa\\Desktop\\catalog.ser");
+        Item doc = new Item("Picture1", "Rota",
+                "D:\\_ user ecaaa\\Desktop\\rota.png");
+        doc.addTag("type", "Slides");
+        catalog.add(doc);
 
-
+        CatalogUtil.save(catalog);
+    }
+    private void testLoadView() throws CatalogUtil.InvalidCatalogException, IOException {
+        Catalog catalog = CatalogUtil.load("D:\\_ user ecaaa\\Desktop\\catalog.ser");
+        assert catalog != null;
+        Item doc = catalog.findById("Picture1");
+        CatalogUtil.view(doc);
     }
 }
