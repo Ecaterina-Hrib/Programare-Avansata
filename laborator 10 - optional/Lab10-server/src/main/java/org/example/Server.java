17d232bf16b4c0eafc4a0459b9d70cbde879327c
Ecaterina Hrib <59749220+Ecaterina-Hrib@users.noreply.github.com>
2021-05-13 14:50:14 +0300

    Update Server.java

--- laborator 10 - optional/Lab10-server/src/main/java/org/example/Server.java
+++ laborator 10 - optional/Lab10-server/src/main/java/org/example/Server.java
@@ -9,12 +9,6 @@ import java.net.SocketTimeoutException;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * <h3> Clasa SocialServer </h3>
- * <p> Defineste portul de conectare al serverului. </p>
- * <p> Serverul asteapta conexiuni din partea clientilor. </p>
- * <p> Pentru fiecare client nou conectat se va crea un fir de executie / thread. </p>
- */
 public class Server {
     public static final int PORT = 2500;
     private static List<ClientThread> clients = new ArrayList();
